e96fb1e968456c9d86e9267d99ac7918d208aadc
Radu Bordea <radu.bordea.dev@gmai.com>
2026-02-01 15:31:35 +0100

    fixk xler login layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,16 +1,10 @@
-// app/sign-in/page.tsx
 "use client";
 import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-background dark:bg-black px-4">
-      <div className="w-full max-w-md p-6 bg-card dark:bg-card-foreground rounded-xl shadow-md border border-border">
-        <h1 className="text-xl font-bold text-foreground dark:text-background mb-4 text-center">
-          Sign In to Shiftoria
-        </h1>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" forceRedirectUrl='/dashboard'/>
-      </div>
+    <div className="min-h-dvh grid place-items-center">
+      <SignIn forceRedirectUrl="/dashboard" />
     </div>
   );
 }
